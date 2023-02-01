f8a18cde3309cfa3c585993ec4a8766f56aed099
pctinkychausky <pctinkychausky@gmail.com>
2023-02-01 23:00:08 +0000

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@/styles/tailwind.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <div className="h-screen bg-gray-900 text-gray-100">
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default App;
